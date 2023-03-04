67a83ffbc2cda55cf4f25bbb1c6f89ce2a8355aa
amirphl <amirphl4@gmail.com>
2023-03-04 04:06:11 +0330

    Implement MarketFeeRates API

--- exchange/wallex/wallex.go
+++ exchange/wallex/wallex.go
@@ -8,14 +8,16 @@ import (
 	"reflect"
 	"strconv"
 
+	"github.com/amirphl/iex/market"
 	"github.com/amirphl/iex/order"
 )
 
 var client http.Client
 
 const (
-	OrderBookURL     = "https://api.wallex.ir/v1/depth?symbol=%s"
-	AllOrderBooksURL = "https://api.wallex.ir/v2/depth/all"
+	orderBookURL     = "https://api.wallex.ir/v1/depth?symbol=%s"
+	allOrderBooksURL = "https://api.wallex.ir/v2/depth/all"
+	feeRateURL       = "https://api.wallex.ir/v1/account/fee"
 )
 
 type order_ struct {
@@ -28,6 +30,13 @@ type orderBook struct {
 	symbol string
 	asks   []order.Order
 	bids   []order.Order
+}
+
+type feeRate struct {
+	symbol        string
+	makerFeeRate  float64
+	takerFeeRate  float64
+	recentDaysSum float64
 }
 
 func (o *order_) Price() float64 {
@@ -52,6 +61,22 @@ func (o *orderBook) Asks() []order.Order {
 
 func (o *orderBook) Bids() []order.Order {
 	return o.bids
+}
+
+func (f *feeRate) Symbol() string {
+	return f.symbol
+}
+
+func (f *feeRate) MakerFeeRate() float64 {
+	return f.makerFeeRate
+}
+
+func (f *feeRate) TakerFeeRate() float64 {
+	return f.takerFeeRate
+}
+
+func (f *feeRate) RecentDaysSum() float64 {
+	return f.recentDaysSum
 }
 
 func sendHTTPRequest(method, url string, body io.Reader, apiKey string) (*http.Response, error) {
@@ -92,6 +117,23 @@ func parseRawOrder(rawOrder reflect.Value) order.Order {
 		price:    price,
 		quantity: quantity,
 		sum:      sum,
+	}
+}
+
+func parseRawFeeRate(rawFeeRate reflect.Value, symbol string) market.FeeRate {
+	rawMaker := rawFeeRate.MapIndex(reflect.ValueOf("makerFeeRate")).Elem()
+	rawTaker := rawFeeRate.MapIndex(reflect.ValueOf("takerFeeRate")).Elem()
+	rawRecentDaysSum := rawFeeRate.MapIndex(reflect.ValueOf("recent_days_sum")).Elem()
+
+	maker, _ := strconv.ParseFloat(rawMaker.String(), 64)
+	taker, _ := strconv.ParseFloat(rawTaker.String(), 64)
+	recentDaysSum := rawRecentDaysSum.Float()
+
+	return &feeRate{
+		symbol:        symbol,
+		makerFeeRate:  maker,
+		takerFeeRate:  taker,
+		recentDaysSum: recentDaysSum,
 	}
 }
 
@@ -137,8 +179,30 @@ func parseRawOrderBooks(rawOrderBooks reflect.Value) []order.OrderBook {
 	return res
 }
 
-func GetOrderBook(symbol string, apiKey string) (order.OrderBook, error) {
-	url := fmt.Sprintf(OrderBookURL, symbol)
+func parseRawFeeRates(rawFeeRates reflect.Value) map[string]market.FeeRate {
+	res := make(map[string]market.FeeRate, rawFeeRates.Len())
+
+	iter := rawFeeRates.MapRange()
+
+	for iter.Next() {
+		symbol := iter.Key().String()
+
+		if symbol == "default" || symbol == "metaData" {
+			// TODO Include in FeeRate?
+			continue
+		}
+
+		rawFeeRate := iter.Value().Elem()
+		feeRate := parseRawFeeRate(rawFeeRate, symbol)
+
+		res[symbol] = feeRate
+	}
+
+	return res
+}
+
+func OrderBook(symbol string, apiKey string) (order.OrderBook, error) {
+	url := fmt.Sprintf(orderBookURL, symbol)
 	resp, err := sendHTTPRequest("GET", url, nil, apiKey)
 
 	if err != nil {
@@ -170,8 +234,8 @@ func GetOrderBook(symbol string, apiKey string) (order.OrderBook, error) {
 	return book, nil
 }
 
-func GetAllOrderBooks(apiKey string) ([]order.OrderBook, error) {
-	url := AllOrderBooksURL
+func AllOrderBooks(apiKey string) ([]order.OrderBook, error) {
+	url := allOrderBooksURL
 	resp, err := sendHTTPRequest("GET", url, nil, apiKey)
 
 	if err != nil {
@@ -201,4 +265,47 @@ func GetAllOrderBooks(apiKey string) ([]order.OrderBook, error) {
 	books := parseRawOrderBooks(refRes)
 
 	return books, nil
+}
+
+func FeeRate(symbol string, apiKey string) (market.FeeRate, error) {
+	feeRates, err := FeeRates(apiKey)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return feeRates[symbol], nil
+}
+
+func FeeRates(apiKey string) (map[string]market.FeeRate, error) {
+	url := feeRateURL
+	resp, err := sendHTTPRequest("GET", url, nil, apiKey)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get fee rates: %d", resp.StatusCode)
+	}
+
+	rawData, err := parseHTTPRespBody(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	refData := reflect.ValueOf(rawData)
+
+	if success := extractKey(refData, "success").Bool(); !success {
+		message := extractKey(refData, "message").String()
+		return nil, fmt.Errorf("failed to get fee rates: %s", message)
+	}
+
+	refRes := extractKey(refData, "result")
+	feeRates := parseRawFeeRates(refRes)
+
+	return feeRates, nil
 }
